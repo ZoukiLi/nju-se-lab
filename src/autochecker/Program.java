5d909da49a4dca93c4f799a3b2d86a0b559a1cd7
zoukiri-r480-debian <zoukiri180@gmail.com>
2022-11-17 04:09:30 +0800

    write Program Compare Interface

--- src/autochecker/Program.java
+++ src/autochecker/Program.java
@@ -1,5 +1,38 @@
 package autochecker;
 
+import autochecker.test.TestBatch;
+import autochecker.test.TestResult;
+import src_reader.SrcReader;
+
 public class Program {
 
+    private final SrcReader srcReader;
+    private boolean _hasCompiled;
+    private TestResult _testResult;
+
+    public Program(SrcReader srcReader) {
+        this.srcReader = srcReader;
+        this._hasCompiled = false;
+    }
+
+    public void compile() {
+        if (this._hasCompiled) {
+            return;
+        }
+
+    }
+
+    public String getPath() {
+        return this.srcReader.getPath();
+    }
+
+    public void runTests(TestBatch testBatch) {
+        if (!this._hasCompiled) {
+            this.compile();
+        }
+    }
+
+    public ComparisonResult compare(Program other) {
+        return ComparisonResult.UNKNOWN;
+    }
 }
