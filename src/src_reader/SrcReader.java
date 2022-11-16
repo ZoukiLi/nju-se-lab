0d4736d5a6af8ea1042feac27420df201a4bae78
zoukiri-r480-debian <zoukiri180@gmail.com>
2022-11-17 05:18:58 +0800

    add copy method for SrcReader

--- src/src_reader/SrcReader.java
+++ src/src_reader/SrcReader.java
@@ -3,6 +3,10 @@ package src_reader;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public abstract class SrcReader {
     public abstract String read();
     public abstract String getPath();
@@ -10,6 +14,14 @@ public abstract class SrcReader {
     private SrcReaderType type;
     public SrcReaderType getType() {
         return type;
+    }
+    public String getTempCopyPath(String prefix) throws IOException {
+        Path tempDir = Files.createTempDirectory(prefix +"-src");
+        Path filePath = Path.of(getPath());
+        Path tempFilePath = tempDir.resolve(filePath.getFileName());
+        //copy the file to the temp dir
+        Files.copy(filePath, tempFilePath);
+        return tempFilePath.toString();
     }
 
     @NotNull
