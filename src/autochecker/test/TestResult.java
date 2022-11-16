a53097a1774e8aa4a33fc6914e0600d004b4afac
zoukiri-r480-debian <zoukiri180@gmail.com>
2022-11-17 03:57:10 +0800

    write TestResult

--- src/autochecker/test/TestResult.java
+++ src/autochecker/test/TestResult.java
@@ -1,4 +1,49 @@
 package autochecker.test;
 
+import java.util.List;
+
+/**
+ * Structure to hold the results of a test.
+ * Both test and results are used for comparison.
+ */
 public class TestResult {
+    private final TestBatch _test;
+    private final List<String> _results;
+    private final List<Boolean> _overTime;
+
+    /**
+     * Create a new TestResult.
+     * @param test the test that was run.
+     * @param results the results of the test.
+     * @param overTime whether the test took too long.
+     */
+    public TestResult(TestBatch test, List<String> results, List<Boolean> overTime) {
+        _test = test;
+        _results = results;
+        _overTime = overTime;
+    }
+
+    /**
+     * Get the test that was run.
+     * @return the test that was run.
+     */
+    public TestBatch getTest() {
+        return _test;
+    }
+
+    /**
+     * Get the results of the test.
+     * @return the results of the test.
+     */
+    public List<String> getResults() {
+        return _results;
+    }
+
+    /**
+     * Get whether the test took too long.
+     * @return whether the test took too long.
+     */
+    public List<Boolean> getOverTime() {
+        return _overTime;
+    }
 }
