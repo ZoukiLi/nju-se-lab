99fef70c38fd6d31427daf946f5bd1027c412667
zoukiri-r480-debian <zoukiri180@gmail.com>
2022-11-17 08:53:36 +0800

    make Comparison Record, and add tostring

--- src/autochecker/Comparison.java
+++ src/autochecker/Comparison.java
@@ -4,29 +4,13 @@ package autochecker;
  * A comparison between two programs.
  * Includes the two programs and the result of the comparison.
  */
-public class Comparison {
-
-    private final Program _program1;
-    private final Program _program2;
-    private final ComparisonResult _result;
+public record Comparison (Program _program1, Program _program2, ComparisonResult _result) {
 
     /**
-     * Create a new Comparison.
-     * @param program1 the first program.
-     * @param program2 the second program.
-     * @param result the result of the comparison.
+     * @return csv representation of this comparison.
      */
-    public Comparison(Program program1, Program program2, ComparisonResult result) {
-        _program1 = program1;
-        _program2 = program2;
-        _result = result;
-    }
-
-    public Program getProgram1() {
-        return _program1;
-    }
-    public Program getProgram2() {
-        return _program2;
+    public String toString() {
+        return _program1.getPath() + "," + _program2.getPath();
     }
     public ComparisonResult getResult() {
         return _result;
