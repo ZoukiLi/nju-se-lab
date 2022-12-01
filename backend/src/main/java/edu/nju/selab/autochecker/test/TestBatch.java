2edec1c6efcb210561bf18993abd7e16165f02bf
zoukiri-r480-debian <zoukiri180@gmail.com>
2022-12-01 23:32:43 +0800

    complete lab 5

--- backend/src/main/java/edu/nju/selab/autochecker/test/TestBatch.java
+++ backend/src/main/java/edu/nju/selab/autochecker/test/TestBatch.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 /**
  * A batch of tests to be run.
+ * @param tests The tests to be run.
+ * @param batchSize The number of tests to run.
  */
 public record TestBatch (List<String> tests, int batchSize) {
     public TestBatch(List<String> tests) {
